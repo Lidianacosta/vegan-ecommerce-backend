43cdecb0e9a97169db4d459f097507ae5b24e3b7
Lidianacosta <lidianacostasouza@gmail.com>
2024-11-25 12:14:43 +0000

    :sparkles: feat: Adding category model and category app.

---
+++ vegan_ecommerce/apps/category/models.py
@@ -0,0 +1,40 @@
+import uuid
+from django.db import models
+from django.utils.translation import gettext_lazy as _
+
+
+class Category(models.Model):
+    id = models.UUIDField(
+        _('id'),
+        default=uuid.uuid4,
+        primary_key=True
+    )
+    name = models.CharField(
+        _('name'),
+        max_length=255,
+        unique=True
+    )
+    slug = models.SlugField(
+        _('slug'),
+        max_length=255,
+        unique=True,
+        null=True
+    )
+    description = models.TextField(_('description'), default='')
+    main_image = models.ImageField(
+        _('main image'),
+        upload_to='category/main_image/%Y/%m',
+        blank=True,
+        null=True,
+        help_text='image that will be displayed on the home page.'
+    )
+    secondary_image = models.ImageField(
+        _('secondary image'),
+        upload_to='category/secondary_image/%Y/%m',
+        blank=True,
+        null=True,
+        help_text='image that will be displayed on the category page.'
+    )
+
+    def __str__(self):
+        return str(self.name)
--- vegan_ecommerce/config/settings/base.py
+++ vegan_ecommerce/config/settings/base.py
@@ -33,6 +33,7 @@ THIRD_PARTY_APPS = [
 
 LOCAL_APPS = [
     "apps.users",
+    "apps.category",
 ]
 
 INSTALLED_APPS = DJANGO_APPS + THIRD_PARTY_APPS + LOCAL_APPS

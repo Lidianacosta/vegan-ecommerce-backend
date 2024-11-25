3d8b63b1bcae7d806c8c5e62805db5869848c09b
Lidianacosta <lidianacostasouza@gmail.com>
2024-11-25 14:31:04 +0000

    :sparkles: feat: Adding custom exception to API.

---
+++ vegan_ecommerce/apps/shared/custom_api_exception.py
@@ -0,0 +1,16 @@
+from rest_framework.exceptions import APIException
+from rest_framework import status
+from django.utils.translation import gettext_lazy as _
+
+
+class CustomApiException(APIException):
+    status_code = status.HTTP_400_BAD_REQUEST
+    default_detail = _('A server error occurred.')
+    default_code = 'error'
+
+    def __init__(self, detail=None, status_code=None):
+        if detail is None:
+            self.detail = {'error': detail}
+        if status_code is None:
+            self.status_code = status_code
+        super().__init__(detail=self.detail)

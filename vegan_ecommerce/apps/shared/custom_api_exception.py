d665537f82924928c9e8e96b729d43862cc86741
Lidianacosta <lidianacostasouza@gmail.com>
2024-11-26 12:56:46 +0000

    :bug: fix: 'Fixing logic in CustomApiException.

--- vegan_ecommerce/apps/shared/custom_api_exception.py
+++ vegan_ecommerce/apps/shared/custom_api_exception.py
@@ -9,8 +9,8 @@ class CustomApiException(APIException):
     default_code = 'error'
 
     def __init__(self, detail=None, status_code=None):
-        if detail is None:
+        if detail is not None:
             self.detail = {'error': detail}
-        if status_code is None:
+        if status_code is not None:
             self.status_code = status_code
         super().__init__(detail=self.detail)

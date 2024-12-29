02f307245348970661f76dd2665b0a1643587bf2
Lidianacosta <lidianacostasouza@gmail.com>
2024-12-29 17:45:38 +0000

    :sparkles: feat: Adding category service

---
+++ vegan_ecommerce/apps/category/service.py
@@ -0,0 +1,56 @@
+from apps.shared.base_service import BaseService
+from apps.shared.custom_api_exception import CustomApiException
+from apps.category.repository import CategoryRepository
+
+
+class CategoryService(BaseService):
+
+    @staticmethod
+    def list_all_instances():
+        try:
+            return CategoryRepository.get_all_instances()
+        except Exception as e:
+            raise CustomApiException(detail=str(e), status_code=500)
+
+    @staticmethod
+    def create_instance(**validated_data):
+        try:
+            return CategoryRepository.create_instance(data=validated_data)
+        except Exception as e:
+            raise CustomApiException(detail=str(e), status_code=500)
+
+    @staticmethod
+    def retrieve_instance(instance_id):
+        try:
+            return CategoryRepository.get_instance_by_id(instance_id=instance_id)
+        except CustomApiException:
+            raise
+        except Exception as e:
+            raise CustomApiException(detail=str(e), status_code=500)
+
+    @staticmethod
+    def update_instance(instance_id, **validated_data):
+        try:
+            return CategoryRepository.update_instance(instance_id=instance_id, data=validated_data)
+        except CustomApiException:
+            raise
+        except Exception as e:
+            raise CustomApiException(detail=str(e), status_code=500)
+
+    @staticmethod
+    def partial_update_instance(instance_id, **validated_data):
+        try:
+            return CategoryRepository.update_instance(instance_id=instance_id, data=validated_data)
+        except CustomApiException:
+            raise
+        except Exception as e:
+            raise CustomApiException(detail=str(e), status_code=500)
+
+    @staticmethod
+    def destroy_instance(instance_id):
+        try:
+            CategoryRepository.delete_instance(instance_id=instance_id)
+        except CustomApiException:
+            raise
+        except Exception as e:
+            raise CustomApiException(detail=str(e), status_code=500)

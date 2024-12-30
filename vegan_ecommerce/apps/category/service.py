deb660296004ee04987a50ff10b8b54ef29c4519
Lidianacosta <lidianacostasouza@gmail.com>
2024-12-30 00:41:28 +0000

    :recycle: refactor: Chaging status code display in category service.

--- vegan_ecommerce/apps/category/service.py
+++ vegan_ecommerce/apps/category/service.py
@@ -1,6 +1,7 @@
 from apps.shared.base_service import BaseService
 from apps.shared.custom_api_exception import CustomApiException
 from apps.category.repository import CategoryRepository
+from rest_framework import status
 
 
 class CategoryService(BaseService):
@@ -10,14 +11,16 @@ class CategoryService(BaseService):
         try:
             return CategoryRepository.get_all_instances()
         except Exception as e:
-            raise CustomApiException(detail=str(e), status_code=500)
+            raise CustomApiException(detail=str(
+                e), status_code=status.HTTP_500_INTERNAL_SERVER_ERROR)
 
     @staticmethod
     def create_instance(**validated_data):
         try:
             return CategoryRepository.create_instance(data=validated_data)
         except Exception as e:
-            raise CustomApiException(detail=str(e), status_code=500)
+            raise CustomApiException(detail=str(
+                e), status_code=status.HTTP_500_INTERNAL_SERVER_ERROR)
 
     @staticmethod
     def retrieve_instance(instance_id):
@@ -26,7 +29,8 @@ class CategoryService(BaseService):
         except CustomApiException:
             raise
         except Exception as e:
-            raise CustomApiException(detail=str(e), status_code=500)
+            raise CustomApiException(detail=str(
+                e), status_code=status.HTTP_500_INTERNAL_SERVER_ERROR)
 
     @staticmethod
     def update_instance(instance_id, **validated_data):
@@ -35,7 +39,8 @@ class CategoryService(BaseService):
         except CustomApiException:
             raise
         except Exception as e:
-            raise CustomApiException(detail=str(e), status_code=500)
+            raise CustomApiException(detail=str(
+                e), status_code=status.HTTP_500_INTERNAL_SERVER_ERROR)
 
     @staticmethod
     def partial_update_instance(instance_id, **validated_data):
@@ -44,7 +49,8 @@ class CategoryService(BaseService):
         except CustomApiException:
             raise
         except Exception as e:
-            raise CustomApiException(detail=str(e), status_code=500)
+            raise CustomApiException(detail=str(
+                e), status_code=status.HTTP_500_INTERNAL_SERVER_ERROR)
 
     @staticmethod
     def destroy_instance(instance_id):
@@ -53,4 +59,5 @@ class CategoryService(BaseService):
         except CustomApiException:
             raise
         except Exception as e:
-            raise CustomApiException(detail=str(e), status_code=500)
+            raise CustomApiException(detail=str(
+                e), status_code=status.HTTP_500_INTERNAL_SERVER_ERROR)

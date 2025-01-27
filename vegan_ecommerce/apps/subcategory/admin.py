465647e4e5f3b2711649b8c1ee3a9be14ff65f28
Lidianacosta <lidianacostasouza@gmail.com>
2025-01-27 12:43:53 +0000

    feat: Adding settings to SubcategoryAdmin.

--- vegan_ecommerce/apps/subcategory/admin.py
+++ vegan_ecommerce/apps/subcategory/admin.py
@@ -5,4 +5,5 @@ from apps.subcategory.models import Subcategory
 
 @admin.register(Subcategory)
 class SubcategoryAdmin(admin.ModelAdmin):
-    ...
+    fields = ('id', 'name', 'slug', 'category', 'description')
+    readonly_fields = ('id', 'slug',)

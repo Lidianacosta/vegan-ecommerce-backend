eded8af4d866fa6fd371bc4d3036a044216d56a3
Lidianacosta <lidianacostasouza@gmail.com>
2025-01-31 17:03:35 +0000

    feat: Changing the bytea_image field that returns your uuid to return your url in the category serializer.

--- vegan_ecommerce/apps/category/api/serializers.py
+++ vegan_ecommerce/apps/category/api/serializers.py
@@ -1,11 +1,22 @@
 from rest_framework import serializers
 from apps.category.models import Category
 
+from apps.bytea_image.service import ByteaImageService
+
 
 class CategorySerializer(serializers.ModelSerializer):
+
+    main_image_url = serializers.SerializerMethodField()
+    secondary_image_url = serializers.SerializerMethodField()
 
     class Meta:
         model = Category
         fields = ('id', 'name', 'slug', 'description',
-                  'main_image', 'secondary_image')
+                  'main_image_url', 'secondary_image_url')
         read_only_fields = ('slug',)
+
+    def get_main_image_url(self, obj):
+        return ByteaImageService.get_image_url(obj.main_image)
+
+    def get_secondary_image_url(self, obj):
+        return ByteaImageService.get_image_url(obj.secondary_image)
